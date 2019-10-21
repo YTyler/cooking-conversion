e5e3371ed451dd4a3014cddd45f996b793202417
Student <example@example.com>
2019-10-21 13:00:03 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var gallonToPint = function(gal) {return gal*8}
+var pintToGallon = function(pint) {return pint/8}
+var gallonToLiter = function(gal) {return gal*3.785}
+var literToGallon = function(lit) {return lit/3.785}
+
+alert(gallonToPint(1))
+alert(pintToGallon(1))
+alert(gallonToLiter(1))
+alert(literToGallon(1))
